3a8b0cd1b8a069985e1281951e69bbb1d097b1b0
Marius Kluften <mkluften93@gmail.com>
2023-07-12 23:31:08 +0200

    add working example of flashing letters to microbit

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,58 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::env;
+use std::fs::File;
+use std::io::Write;
+use std::process::Command;
+use std::time::Duration;
+use wait_timeout::ChildExt;
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+// #[tauri::command]
+// fn greet(name: &str) -> String {
+//     format!("Hello, {}! You've been greeted from Rust!", name)
+// }
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}", name)
+fn flash_display_nrf(commands: &str) -> String {
+    println!("{:?}", commands);
+    let target_dir = "../src-microbit/attempts/attempt2.json";
+
+    File::create(target_dir)
+        .unwrap()
+        .write_all(commands.as_bytes())
+        .expect("Error writing to file");
+
+    let one_sec = Duration::from_secs(2);
+
+    let mut child = Command::new("sh")
+        .current_dir("../src-microbit")
+        .arg("-c")
+        .arg("cargo rb input_from_build")
+        //.arg("probe-run --list-probes")
+        .spawn()
+        .expect("ls command failed to start");
+
+    let status_code = match child.wait_timeout(one_sec).unwrap() {
+        Some(status) => status.code(),
+        None => {
+            println!("None in status_code");
+            child.kill().unwrap();
+            child.wait().unwrap().code()
+        }
+    };
+
+    if status_code.is_some() {
+        println!("{:?}", status_code);
+    }
+
+    format!("Hello, {}! You've been greeted from Rust!", commands)
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![flash_display_nrf])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/lib/Greet.svelte
+++ src/lib/Greet.svelte
@@ -1,16 +1,37 @@
-<script>
-	import { invoke } from '@tauri-apps/api/tauri';
+<script lang="ts">
+	import { invoke } from '@tauri-apps/api/tauri'
+	import letters from '../letters.json'
 
-	let name = '';
-	let greetMsg = '';
+	let greetMsg = ''
 
-	async function greet() {
-		greetMsg = await invoke('greet', { name });
+	let alphabet = Object.keys(letters)
+
+	async function greet(letter: string) {
+		// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+		let commands = JSON.stringify({
+			age: 130,
+			display: letters[letter || 'A'],
+		})
+		alert(`hey ${letter}`)
+		greetMsg = await invoke('flash_display_nrf', { commands })
 	}
 </script>
 
 <div>
-	<input id="greet-input" placeholder="Enter a name..." bind:value={name} />
-	<button on:click={greet}>Greet</button>
+	<div class="alphabet">
+		{#each alphabet as letter}
+			<button on:click={() => greet(letter)}>
+				{letter}
+			</button>
+		{/each}
+	</div>
+
 	<p>{greetMsg}</p>
 </div>
+
+<style>
+	.alphabet {
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+	}
+</style>
