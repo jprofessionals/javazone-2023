3b98c47ce98fb348c3509ee8de7437c96f53684b
Marius Kluften <mkluften93@gmail.com>
2023-07-14 21:48:44 +0200

    rename greet to flash

--- app/src/lib/FlashMicrobit.svelte
+++ app/src/lib/FlashMicrobit.svelte
@@ -1,33 +1,41 @@
 <script lang="ts">
 	import { invoke } from '@tauri-apps/api/tauri'
-	import letters from '../letters.json'
+	import _letters from '../letters.json'
 
 	let greetMsg = ''
 
+	const letters = _letters as { [key: string]: number[][] }
+
 	let alphabet = Object.keys(letters)
 
-	async function greet(letter: string) {
+	async function flash_microbit(letter: string) {
 		// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 		let commands = JSON.stringify({
 			age: 130,
 			display: letters[letter || 'A'],
 		})
+
 		alert(`hey ${letter}`)
 		greetMsg = await invoke('flash_display_nrf', { commands })
 	}
+
+	let message: string[] = []
 </script>
 
-<div>
-	<div class="alphabet">
-		{#each alphabet as letter}
-			<button on:click={() => greet(letter)}>
-				{letter}
-			</button>
-		{/each}
-	</div>
-
-	<p>{greetMsg}</p>
+<div class="alphabet">
+	{#each alphabet as letter}
+		<button
+			on:click={() => {
+				message = [...message, letter]
+			}}
+		>
+			{letter}
+		</button>
+	{/each}
+	{message.join('')}
 </div>
+
+<p>{greetMsg}</p>
 
 <style>
 	.alphabet {
