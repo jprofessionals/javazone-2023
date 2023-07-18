b22b845e65b9ef0b78047f537f260df1680f010e
Marius Kluften <mkluften93@gmail.com>
2023-07-18 15:29:04 +0200

    add debug subpage

---
+++ app/src/routes/debug/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import BluetoothDebugger from '$lib/BluetoothDebugger.svelte'
+	import BluetoothScan from '$lib/BluetoothScan.svelte'
+</script>
+
+<div class="p-10 flex flex-col gap-4">
+	<div class="card p-4">
+		<h2>Bluetooth scanner</h2>
+		<BluetoothScan />
+	</div>
+	<div class="card p-4">
+		<h2>Send message to bluetooth device</h2>
+		<BluetoothDebugger />
+	</div>
+</div>
