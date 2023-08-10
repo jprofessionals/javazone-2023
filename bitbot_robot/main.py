95aa498e3d3f5be505a2a2c5bdea39b128ed5403
Frode Roxrud Gill <frodegill@jpro.no>
2023-08-10 16:30:51 +0200

    Working RFID

--- bitbot_robot/main.py
+++ bitbot_robot/main.py
@@ -4,12 +4,12 @@ import math
 import neopixel
 import radio
 
-class RFIDState:
+
+class RFIDCom:
     READY = 1
-    PINGED = 2
-    PING_COMPLETED = 3
-    ACKED = 4
-    ACK_COMPLETED = 5
+    WAITING_FOR_ACK = 2
+    WAITING_FOR_RESPONSE = 3
+
 
 _PREAMBLE = const(0x00)
 _STARTCODE1 = const(0x00)
@@ -19,26 +19,24 @@ _POSTAMBLE = const(0x00)
 _HOSTTOPN532 = const(0xD4)
 _PN532TOHOST = const(0xD5)
 
-# PN532 Commands
 _COMMAND_SAMCONFIGURATION = const(0x14)
+_COMMAND_RFCONFIGURATION = const(0x32)
 _COMMAND_INLISTPASSIVETARGET = const(0x4A)
 
 _MIFARE_ISO14443A = const(0x00)
 
-# Mifare Commands
-MIFARE_CMD_READ = const(0x30)
-MIFARE_CMD_WRITE = const(0xA0)
-MIFARE_ULTRALIGHT_CMD_WRITE = const(0xA2)
-
-_ACK = b'\x00\x00\xFF\x00\xFF\x00'
-_FRAME_START = b'\x00\x00\xFF'
+_ACK = b"\x00\x00\xFF\x00\xFF\x00"
+_FRAME_START = b"\x00\x00\xFF"
 
 _I2C_ADDRESS = const(0x24)
-_NOT_BUSY = const(0x01)
+_I2C_READY = const(0x01)
+
+_I2C_DELAY = 10
 
 
 class BusyError(Exception):
     pass
+
 
 class PN532:
     def __init__(self, i2c, *, irq=None, req=None, debug=False):
@@ -46,51 +44,20 @@ class PN532:
         self._irq = irq
         self._req = req
         self._i2c = i2c
-        self.state = RFIDState.READY
+        self.state = RFIDCom.READY
         self.initialised = False
+        self.configured = False
+        self.previousCommand = None
+        self.previousCommandTime = 0
         return
 
-    def _read_data(self, count):
-        if self.debug:
-            print("_rd")
-
-        status_byte = self._i2c.read(_I2C_ADDRESS, 1)
-        if self.debug:
-            print("_rd status_byte: ", status_byte)
-        if status_byte[0] != _NOT_BUSY:             # not ready
-            if self.debug:
-                print("_rd busy_error ")
-            raise BusyError
-        if self.debug:
-            print("_rd readfrom_into")
-        frame = self._i2c.read(_I2C_ADDRESS, count)
-        if self.debug:
-            print("_rd frame: ", frame)
-        return frame
-
-    def _write_data(self, framebytes):
-        try:
-            if self.debug:
-                print('_wd: ', [hex(i) for i in framebytes])
-            self._i2c.write(_I2C_ADDRESS, framebytes)
-        except (OSError, RuntimeError):
-            if self.debug:
-                print("is_ready exception")
+    def _write_data(self, frame):
+        print("write: ", [hex(i) for i in frame])
+        self._i2c.write(_I2C_ADDRESS, frame)
 
     def _write_frame(self, data):
-        """Write a frame to the PN532 with the specified data bytearray."""
-        assert data is not None and 1 < len(
-            data) < 255, 'Data must be 1-255 bytes.'
-        # Build frame to send as:
-        # - Preamble (0x00)
-        # - Start code  (0x00, 0xFF)
-        # - Command length (1 byte)
-        # - Command length checksum
-        # - Command bytes
-        # - Checksum
-        # - Postamble (0x00)
         length = len(data)
-        frame = bytearray(length+8)
+        frame = bytearray(length + 7)
         frame[0] = _PREAMBLE
         frame[1] = _STARTCODE1
         frame[2] = _STARTCODE2
@@ -98,126 +65,119 @@ class PN532:
         frame[3] = length & 0xFF
         frame[4] = (~length + 1) & 0xFF
         for x in range(length):
-            frame[5+x] = data[x]
-        print('Frame:', [hex(i) for i in frame])
+            frame[5 + x] = data[x]
         checksum += sum(data)
-        frame[-2] = (~checksum & 0xFF)+1
+        frame[-2] = ~checksum & 0xFF
         frame[-1] = _POSTAMBLE
-        # Send frame.
-        if self.debug:
-            print('Write frame: ', [hex(i) for i in frame])
         self._write_data(bytes(frame))
 
     def write_command(self, command, params=[]):
-        data = bytearray(2+len(params))
+        data = bytearray(2 + len(params))
         data[0] = _HOSTTOPN532
         data[1] = command & 0xFF
         for i, val in enumerate(params):
-            data[2+i] = val
+            data[2 + i] = val
+        self._write_frame(data)
+        return command
 
-        try:
-            self._write_frame(data)
-        except OSError:
-            if self.debug:
-                print("write_command OSError")
+    def _read_data(self, count):
+        frame = self._i2c.read(_I2C_ADDRESS, count + 1)
+        if frame[0] != _I2C_READY:
+            raise BusyError
+        print("read: ", [hex(i) for i in frame])
+        return frame[1:]
 
     def _read_frame(self, length):
-        response = self._read_data(length+8)
-        if self.debug:
-            print('Read frame:', [hex(i) for i in response])
-
-        # Swallow all the 0x00 values that preceed 0xFF.
-        offset = 0
-        while response[offset] == 0x00:
-            offset += 1
-            if offset >= len(response):
-                raise RuntimeError(
-                    'Response frame preamble does not contain 0xFF!')
-        if response[offset] != 0xFF:
-            raise RuntimeError(
-                'Response frame preamble does not contain 0xFF!')
-        offset += 1
-        if offset >= len(response):
-            raise RuntimeError('Response contains no data!')
+        response = self._read_data(length + 8)
+        if response[0:3] != _FRAME_START:
+            raise RuntimeError("Invalid response frame start")
         # Check length & length checksum match.
-        frame_len = response[offset]
-        if (frame_len + response[offset+1]) & 0xFF != 0:
-            raise RuntimeError(
-                'Response length checksum mismatch')
+        frame_len = response[3]
+        if (frame_len + response[4]) & 0xFF != 0:
+            raise RuntimeError("Response length checksum mismatch")
         # Check frame checksum value matches bytes.
-        checksum = sum(response[offset+2:offset+2+frame_len+1]) & 0xFF
+        checksum = sum(response[5 : 5 + frame_len + 1]) & 0xFF
         if checksum != 0:
-            raise RuntimeError(
-                'Response checksum mismatch:', checksum)
+            raise RuntimeError("Response checksum mismatch:", checksum)
         # Return frame data.
-        return response[offset+2:offset+2+frame_len]
+        return response[5 : 5 + frame_len]
 
     def is_ready(self):
         status = bytearray(1)
         try:
             status = self._i2c.read(_I2C_ADDRESS, 1)
         except OSError:
-            if self.debug:
-                print("is_ready exception")
-        if status != b'\x00':
-            print("status:", status)
-        return status == b'\x01'
+            return False
+        return status == b"\x01"
 
     def got_ack(self):
         return self._read_data(len(_ACK)) == _ACK
 
-    def get_card_id(self, command, response_length=0):
-        response = self._read_frame(response_length+2)
-        if not (response[0] == _PN532TOHOST and response[1] == (command+1)):
-            raise RuntimeError('Received unexpected command response!')
-        return response[2:]
+    def get_card_id(self, command, response_length):
+        print("get_card_id")
+        response = self._read_frame(response_length + 2)
+        print("get_card_id: ", [hex(i) for i in response])
+        if not (response[0] == _PN532TOHOST and response[1] == (command + 1)):
+            print("Invalid card response")
+            raise RuntimeError("Invalid card response")
+        # Check only 1 card with up to a 7 byte UID is present.
+        if response[2] != 0x01 or response[7] > 7:
+            print("Unsupported card response")
+            raise RuntimeError("Unsupported card response")
+        return response[8 : 8 + response[7]]
 
     def handle_rfid(self):
-        # return a bytearray with the UID if a card is detected.
-        if self.state == RFIDState.READY:
-            if not self.initialised:
-                # - 0x01, normal mode
-                # - 0x02, timeout 50ms * 2 = 100ms
-                # - 0x01, use IRQ pin
-                self.write_command(_COMMAND_SAMCONFIGURATION,
-                                   params=[0x01, 0x02, 0x01])
-            else:
-                print("ping card")
-                self.write_command(_COMMAND_INLISTPASSIVETARGET,
-                                   params=[0x01, _MIFARE_ISO14443A])
-            self.state = RFIDState.PINGED
-        elif self.state == RFIDState.PINGED:
-            if self.is_ready():
-                self.state = RFIDState.PING_COMPLETED
-        elif self.state == RFIDState.PING_COMPLETED:
-            print("ping completed")
-            if self.got_ack() is True:
-                self.state = RFIDState.ACKED
-        elif self.state == RFIDState.ACKED:
-            print("acked")
-            if self.is_ready():
-                self.state = RFIDState.ACK_COMPLETED
-        elif self.state == RFIDState.ACK_COMPLETED:
-            if not self.initialised:
-                print("init completed")
-                self.initialised = True
-                self.state = RFIDState.READY
-            else:
-                response = self.get_card_id(_COMMAND_INLISTPASSIVETARGET,
-                                            response_length=19)
-                if response is None:
-                    return None
+        try:
+            currentRFIDTime = running_time()
+            if (self.previousCommandTime + _I2C_DELAY) > currentRFIDTime:
+                return None
 
-                # Check only 1 card with up to a 7 byte UID is present.
-                if response[0] != 0x01:
-                    raise RuntimeError('>1 card detected!')
-                if response[5] > 7:
-                    raise RuntimeError('Found card with long UID!')
+            if self.state != RFIDCom.READY and not self.is_ready():
+                return None
 
-                print("card found")
-                self.state = RFIDState.READY
-                # Return UID of card.
-                return response[6:6+response[5]]
+            self.previousCommandTime = currentRFIDTime
+
+            if self.state == RFIDCom.READY:
+                if self.previousCommand is None:
+                    self.previousCommand = self.write_command(
+                        _COMMAND_SAMCONFIGURATION, params=[0x01, 0x00, 0x01]
+                    )
+                elif self.previousCommand is _COMMAND_SAMCONFIGURATION:
+                    self.previousCommand = self.write_command(
+                        _COMMAND_RFCONFIGURATION, params=[0x01, 0x01]
+                    )
+                else:
+                    self.previousCommand = self.write_command(
+                        _COMMAND_INLISTPASSIVETARGET, params=[0x01, _MIFARE_ISO14443A]
+                    )
+                    # self.write_command(0x60,
+                    #                    params=[0x01, 0x01, _MIFARE_ISO14443A])
+                self.state = RFIDCom.WAITING_FOR_ACK
+            elif self.state == RFIDCom.WAITING_FOR_ACK:
+                if self.got_ack():
+                    self.state = RFIDCom.WAITING_FOR_RESPONSE
+            elif self.state == RFIDCom.WAITING_FOR_RESPONSE:
+                if self.previousCommand is _COMMAND_SAMCONFIGURATION:
+                    self._read_frame(0)
+                    print('SAMConfiguration complete')
+                elif self.previousCommand is _COMMAND_RFCONFIGURATION:
+                    self._read_frame(0)
+                    print('RFConfiguration complete')
+                elif self.previousCommand is _COMMAND_INLISTPASSIVETARGET:
+                    print("got card?")
+                    response = self.get_card_id(
+                        _COMMAND_INLISTPASSIVETARGET, response_length=19
+                    )
+                    if response is not None:
+                        print("read: ", [hex(i) for i in response])
+                        print("card found: ", response)
+                        self.state = RFIDCom.READY
+                        return response
+                self.state = RFIDCom.READY
+        except (OSError, RuntimeError, BusyError):
+            pass
+        return None
+
 
 class Command:
     def __init__(self, opcode, duration, useLeftMotor, useRightMotor):
@@ -226,7 +186,8 @@ class Command:
         self.useLeftMotor = useLeftMotor
         self.useRightMotor = useRightMotor
 
-speed = 277.0/1000.0  # mm/ms
+
+speed = 277.0 / 1000.0  # mm/ms
 wheelbase = 117.0  # mm between robot wheels
 breakTime = 300  # ms it takes to stop robot
 
@@ -245,25 +206,30 @@ mostRecentTagTime = 0
 
 fireleds = neopixel.NeoPixel(pin13, 12)
 
+
 def degreesToLength(degrees):
-    return wheelbase*2*(math.pi)*(degrees/360.0)
+    return wheelbase * 2 * (math.pi) * (degrees / 360.0)
+
 
 def lengthToTime(length):
-    return length/speed
+    return length / speed
+
 
 def drive(useLeftMotor, useRightMotor):
     if False:  # useLeftMotor:
-        pin16.write_analog(torque*leftAdjust)
+        pin16.write_analog(torque * leftAdjust)
         pin8.write_digital(0)
 
     if False:  # useRightMotor:
-        pin14.write_analog(torque*rightAdjust)
+        pin14.write_analog(torque * rightAdjust)
         pin12.write_digital(0)
+
 
 def setLEDs(brightness):
     for pixel_id in range(0, 11):
-        fireleds[pixel_id] = (0, int(255.0*brightness), 0)
+        fireleds[pixel_id] = (0, int(255.0 * brightness), 0)
     fireleds.show()
+
 
 def stop():
     # Left motor
@@ -273,17 +239,22 @@ def stop():
     pin14.write_digital(0)
     pin12.write_digital(0)
 
+
 def appendForward(length):
-    commands.append(Command("forward", lengthToTime(length),
-                            True, True))
+    commands.append(Command("forward", lengthToTime(length), True, True))
+
 
 def appendLeft(degrees):
-    commands.append(Command("left", lengthToTime(degreesToLength(degrees)),
-                            False, True))
+    commands.append(
+        Command("left", lengthToTime(degreesToLength(degrees)), False, True)
+    )
+
 
 def appendRight(degrees):
-    commands.append(Command("right", lengthToTime(degreesToLength(degrees)),
-                            True, False))
+    commands.append(
+        Command("right", lengthToTime(degreesToLength(degrees)), True, False)
+    )
+
 
 def initializeNextRun():
     setLEDs(0.0)
@@ -305,14 +276,24 @@ def initializeNextRun():
     appendLeft(90.0)
     appendForward(200.0)
 
+
 def endRun():
     stop()
     display.off()
 
+
 radio.config(channel=7, power=7)
 radio.on()
 
+i2c.init()
+if _I2C_ADDRESS in i2c.scan():
+    print("Found PN532")
+else:
+    print("PN532 NOT found!!!")
+
 pn532 = PN532(i2c, debug=True)
+while True:
+    pn532.handle_rfid()
 
 while True:
     initializeNextRun()
@@ -325,7 +306,7 @@ while True:
         runningTime = running_time()
 
         # Exit run if it has used up allowed time
-        if (runningTime >= (currentGameStartTime + gameTime)):
+        if runningTime >= (currentGameStartTime + gameTime):
             break
 
         # Count down seconds remaining
@@ -354,9 +335,10 @@ while True:
 
         pn532.handle_rfid()
         # Light up LEDs if tag is found
-        if (runningTime <= (mostRecentTagTime + tagDisplayTime)):
-            setLEDs(((mostRecentTagTime + tagDisplayTime) - runningTime)
-                    / tagDisplayTime)
+        if runningTime <= (mostRecentTagTime + tagDisplayTime):
+            setLEDs(
+                ((mostRecentTagTime + tagDisplayTime) - runningTime) / tagDisplayTime
+            )
 
     endRun()
     break
