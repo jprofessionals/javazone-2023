4eea2163f6e90b2dd416b6f4901d7b33166b2053
Marius Kluften <mkluften93@gmail.com>
2023-08-14 13:39:50 +0200

    update editor to be more "script"-like in its syntax. Also hijack skeleton Codeblock in with custom name for block

--- app/src/lib/Editor.svelte
+++ app/src/lib/Editor.svelte
@@ -1,17 +1,34 @@
 <script lang="ts">
 	import cn from '$utils/cn'
+	import { invoke } from '@tauri-apps/api/tauri'
 
-	type Direction = 'LEFT' | 'FORWARD' | 'RIGHT'
+	const valid_instructions = [
+		'move_forward()j',
+		'move_left()j',
+		'move_right()j',
+		'u_turn()j',
+		'start()j',
+	] as const
+
+	type Instruction = (typeof valid_instructions)[number]
 	type CodeLine = {
-		text: [Direction, string] | ['START']
+		text: [Instruction, string] | ['start()j']
 		error?: boolean
+		byte_value?: 'F' | 'L' | 'R' | 'U' | 'S'
+	}
+	const byte_value: Record<Instruction, 'F' | 'L' | 'R' | 'U' | 'S'> = {
+		'move_forward()j': 'F',
+		'move_left()j': 'L',
+		'move_right()j': 'R',
+		'u_turn()j': 'U',
+		'start()j': 'S',
 	}
 
 	const example = [
-		{ text: ['FORWARD', '2'] },
-		{ text: ['LEFT', '90'] },
-		{ text: ['FORWARD', '10'] },
-		{ text: ['START'] },
+		{ text: ['move_forward()j'], byte_value: 'F' },
+		{ text: ['move_left()j'], byte_value: 'L' },
+		{ text: ['move_forward()j'], byte_value: 'F' },
+		{ text: ['start()j'], byte_value: 'S' },
 	] as CodeLine[]
 
 	let code: CodeLine[] = example
@@ -26,62 +43,69 @@
 		numOfLines = text.length
 		code = parseText(text)
 	}
+
 	const parseText = (code: string[]): CodeLine[] =>
 		code.map((line) => {
-			const splitLine = line.split(' ')
-			if (splitLine.length === 1 && splitLine[0].toUpperCase() === 'START') {
+			const splitLine = line.split('*')
+			// Valid instruction
+			if (
+				splitLine.length === 1 &&
+				valid_instructions.includes(splitLine[0].toLowerCase() as Instruction)
+			) {
+				const instruction = splitLine[0] as Instruction
 				return {
 					text: splitLine,
+					byte_value: byte_value[instruction],
 				} as CodeLine
 			} else if (splitLine.length === 1 && splitLine[0].trim() === '') {
 				return {
 					text: splitLine,
 				} as CodeLine
-			} else if (splitLine.length === 2) {
-				const [dir, val] = splitLine
-				if (['FORWARD', 'LEFT', 'RIGHT'].includes(dir.toUpperCase()) && !isNaN(Number(val))) {
-					return {
-						text: splitLine,
-					} as CodeLine
-				}
+				// Found multiplier, indicating multiple instructions on same line
 			}
 			return {
 				text: splitLine,
 				error: true,
 			} as CodeLine
 		})
+
+	const sendToBitBot = async () => {
+		console.log(code.filter(({ error }) => error).length)
+		if (code.filter(({ error }) => error).length > 0) return
+		const msg = code
+			.filter((line) => line.byte_value !== undefined)
+			.map(({ byte_value }) => byte_value)
+			.join('')
+		await invoke('connect_to_selected_port', { port: '/dev/tty.usbmodem11402', message: msg })
+	}
 </script>
 
-Write your program here:
-<div class="flex">
-	<div class="m-10 w-fit flex flex-col items-end gap-10">
-		<div class="flex gap-2 p-2 bg-slate-700 w-fit rounded-xl">
-			<div class="flex flex-col text-slate-300 text-sm">
-				{#each Array(Math.max(1, numOfLines)) as _, index}
-					<span class="leading-6">
-						{index + 1}
-					</span>
-				{/each}
-			</div>
-			<textarea
-				on:keyup={textareaOnKeyup}
-				class="flex leading-6 px-2 bg-slate-700 text-white border-none outline-none w-96"
-				value={currentText}
-				rows={15}
-			/>
-			<div class="flex flex-col text-slate-300 text-sm">
-				{#each code as line}
-					<span class={cn('leading-6 bg-green-300 h-6 w-2', line.error && 'bg-red-600')} />
-				{/each}
-			</div>
+<div class="flex flex-col gap-10 items-center">
+	<h2 class="h3">Write your program here:</h2>
+	<div class="flex gap-2 p-2 bg-slate-700 rounded-xl">
+		<div class="flex flex-col text-slate-300 text-sm">
+			{#each Array(Math.max(1, numOfLines)) as _num, index}
+				<span class="leading-6">
+					{index + 1}
+				</span>
+			{/each}
 		</div>
-		<button class="btn variant-filled">Send to bitbot</button>
+		<textarea
+			on:keyup={textareaOnKeyup}
+			class="flex leading-6 px-2 bg-slate-700 text-white border-none outline-none w-full"
+			value={currentText}
+			rows={15}
+		/>
+		<div class="flex flex-col text-slate-300 text-sm">
+			{#each code as line}
+				<span
+					class={cn(
+						'first:rounded-tr-lg last:rounded-br-lg leading-6 bg-green-300 h-6 w-2',
+						line.error && 'bg-red-600',
+					)}
+				/>
+			{/each}
+		</div>
 	</div>
-	<div class="card w-80 p-4 mt-10 bg-amber-100 parch prose">
-		<p>Instruction manual</p>
-		<p>Forward: X amount of cm's to move forward</p>
-		<p>Left: X amount of degrees to move to the left</p>
-		<p>Right: X amount of degrees to move to the right</p>
-		<p>Start: Kick off the program</p>
-	</div>
+	<button on:click={sendToBitBot} class="btn variant-filled">Send to bitbot</button>
 </div>
