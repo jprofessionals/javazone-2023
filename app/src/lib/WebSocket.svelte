41c5a7edc0ea7481a5df81ec1a7efa28e5f3e1a6
Marius Kluften <mkluften93@gmail.com>
2023-08-16 14:39:56 +0200

    add generic websocket module

---
+++ app/src/lib/WebSocket.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import { onMount } from 'svelte'
+	import WebSocket from 'tauri-plugin-websocket-api'
+	import type { Message } from 'tauri-plugin-websocket-api'
+
+	export let onListen: (message: Message) => void = updateLog
+	export let shouldDisplayLog: boolean = true
+
+	let log: Array<string | Message> = []
+	let ws: WebSocket | void
+	onMount(async () => {
+		ws = await WebSocket.connect('ws://127.0.0.1:8080')
+			.then((r) => {
+				updateLog('connected')
+				return r
+			})
+			.catch(console.log)
+		if (!ws) return
+		ws.addListener(onListen)
+	})
+
+	export async function send(message: Message | string) {
+		if (ws) {
+			try {
+				await ws.send(message)
+				updateLog('Message sent')
+				updateLog(message)
+			} catch (error: unknown) {
+				updateLog(error as string)
+			}
+		}
+	}
+	function updateLog(entry: Message | string) {
+		if (!shouldDisplayLog) return
+
+		log = [...log, entry]
+	}
+</script>
+
+{#if shouldDisplayLog}
+	<h1>Websocket log:</h1>
+
+	<ul>
+		{#each log as item}
+			<li>{JSON.stringify(item)}</li>
+		{/each}
+	</ul>
+{/if}
