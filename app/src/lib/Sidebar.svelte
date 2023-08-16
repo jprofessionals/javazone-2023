36b989f86efbef4bb45c3b5a86a79e75ab9e404a
Marius Kluften <mkluften93@gmail.com>
2023-08-16 14:44:13 +0200

    clean up sidebar

--- app/src/lib/Sidebar.svelte
+++ app/src/lib/Sidebar.svelte
@@ -18,29 +18,16 @@
 			<span>Play</span>
 		</div>
 	</AppRailAnchor>
-	<AppRailAnchor href="/ws" selected={$page.url.pathname === '/ws'}>
-		<div class="flex flex-col">
-			<span class="text-3xl leading-none">🌐🧦</span>
-			<span>WebSocket</span>
-		</div>
-	</AppRailAnchor>
 	<AppRailAnchor href="/leaderboard" selected={$page.url.pathname === '/leaderboard'}>
 		<div class="flex flex-col">
 			<span class="text-3xl leading-none">🤩</span>
 			<span>Leaderboard</span>
 		</div>
 	</AppRailAnchor>
-	<AppRailAnchor href="/microbit" selected={$page.url.pathname === '/microbit'}>
-		<div class="flex flex-col">
-			<span class="text-3xl leading-none">🤖</span>
-			<span>Microbit</span>
-		</div>
-	</AppRailAnchor>
-	<!-- <svelte:fragment slot="trail"> -->
-	<!-- 	<AppRailAnchor href="/debug" title="Debug" selected={$page.url.pathname === '/debug'}> -->
-	<!-- 		<div class="flex flex-col"> -->
-	<!-- 			<span class="text-3xl leading-none">🐛</span> -->
-	<!-- 		</div> -->
-	<!-- 	</AppRailAnchor> -->
-	<!-- </svelte:fragment> -->
+	<!-- <AppRailAnchor href="/microbit" selected={$page.url.pathname === '/microbit'}> -->
+	<!-- 	<div class="flex flex-col"> -->
+	<!-- 		<span class="text-3xl leading-none">🤖</span> -->
+	<!-- 		<span>Microbit</span> -->
+	<!-- 	</div> -->
+	<!-- </AppRailAnchor> -->
 </AppRail>
