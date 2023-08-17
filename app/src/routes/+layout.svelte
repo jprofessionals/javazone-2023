c6069b63d5343ae97977dd00e7c61c205855769e
Marius Kluften <mkluften93@gmail.com>
2023-08-18 00:10:33 +0200

    move logic for usb device to config page

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -12,32 +12,6 @@
 	import { AppShell } from '@skeletonlabs/skeleton'
 	import Header from './Header.svelte'
 	import Sidebar from '$lib/Sidebar.svelte'
-
-	import { usbDevice } from '$lib/stores'
-	import { onMount } from 'svelte'
-	import { invoke } from '@tauri-apps/api/tauri'
-
-	let usbDeviceValue: string
-
-	usbDevice.subscribe((val) => {
-		usbDeviceValue = val
-	})
-
-	let availableDevices: string[] = []
-
-	onMount(async () => {
-		const foundPorts = ((await invoke('get_available_ports')) || []) as string[]
-		availableDevices = foundPorts.filter((port: string) => !port.includes('Bluetooth'))
-	})
-
-	let selectedDevice: string
-
-	const setDevice = async () => {
-		if (!selectedDevice) return
-		await invoke('set_device', { device: selectedDevice })
-		const device = (await invoke('get_device')) as string
-		usbDevice.set(device)
-	}
 </script>
 
 <AppShell>
@@ -52,18 +26,6 @@
 
 	<!-- <svelte:fragment slot="pageFooter">Footer</svelte:fragment> -->
 
-	<div class="absolute left-2 bottom-0 bg-orange-200 p-2 rounded-t-lg flex items-end gap-4">
-		<label class="label">
-			<span>Select device:</span>
-			<select class="select" bind:value={selectedDevice}>
-				<option>Unset</option>
-				{#each availableDevices as device}
-					<option value={device} selected={device === usbDeviceValue}>{device}</option>
-				{/each}
-			</select>
-		</label>
-		<button class="btn variant-filled" on:click={setDevice}>Connect</button>
-	</div>
 	<div class="absolute -right-4 -bottom-4">
 		<img src={Img} alt="dog" width="200" />
 	</div>
