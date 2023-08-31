3cfd2abc63192686e4499ad3dd922db39f041dae
Marius Kluften <mkluften93@gmail.com>
2023-08-31 09:59:53 +0200

    migrate to skeleton v2

---
+++ web_app/tailwind.config.ts
@@ -0,0 +1,32 @@
+import { join } from 'path'
+import type { Config } from 'tailwindcss'
+
+import { skeleton } from '@skeletonlabs/tw-plugin'
+import forms from '@tailwindcss/forms'
+
+const config = {
+	darkMode: 'class',
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}'),
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				fancy: ['Poppins'],
+			},
+			screens: {
+				'3xl': '2560px',
+			},
+			width: {
+				dscreen: '100dvw',
+			},
+			height: {
+				dscreen: '100dvh',
+			},
+		},
+	},
+	plugins: [forms, skeleton({ themes: { preset: ['skeleton', 'wintry'] } })],
+} satisfies Config
+
+export default config
