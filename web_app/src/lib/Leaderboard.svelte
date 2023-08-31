e25ba5180a6446d13f1ab1ac64a44771e0a63100
Marius Kluften <mkluften93@gmail.com>
2023-08-31 10:00:37 +0200

    make leaderboard take the whole screen

--- web_app/src/lib/Leaderboard.svelte
+++ web_app/src/lib/Leaderboard.svelte
@@ -20,31 +20,44 @@
 	// currentHighScore.subscribe((hs) => {
 	// 	highScoreData = hs
 	// })
+	//
 </script>
 
 <svelte:head>
 	<title>JProZone 2023</title>
 </svelte:head>
 
-<div class="container h-full flex items-center flex-col gap-16 pt-32">
-	<h1 class="h1">Current Leaderboard</h1>
-
-	<ol class="text-4xl flex flex-col gap-4 bg-black p-10 starry">
-		{#each highScoreData as highscore, index}
-			<li
-				class={cn(
-					'text-white',
-					index === 0 && 'text-[#FFD700]',
-					index === 1 && 'text-[#d0d0d0]',
-					index === 2 && 'text-[#cd7f32]',
-					'[text-shadow:_4px_4px_0_rgb(100_100_100_/_60%)]',
-					'flex gap-10 justify-between pixel-font',
-				)}
-			>
-				<span>{highscore.players.username}</span>
-				<!-- Pad the score with 0's -->
-				<span>{String(highscore.score).padStart(3, '0')}</span>
-			</li>
-		{/each}
-	</ol>
+<div class={'container max-w-full h-full'}>
+	<div
+		class={cn(
+			'text-4xl flex flex-col gap-12 bg-black starry relative items-center py-10',
+			'w-full h-full',
+		)}
+	>
+		<h1
+			class={cn(
+				'h1 max-w-lg text-white text-center [text-shadow:_4px_4px_0_rgb(100_100_100_/_60%)] pixel-font',
+			)}
+		>
+			Current Leaderboard
+		</h1>
+		<ol class={cn('text-4xl flex flex-col gap-4 bg-black starry relative')}>
+			{#each highScoreData as highscore, index}
+				<li
+					class={cn(
+						'text-white',
+						index === 0 && 'text-[#FFD700]',
+						index === 1 && 'text-[#d0d0d0]',
+						index === 2 && 'text-[#cd7f32]',
+						'[text-shadow:_4px_4px_0_rgb(100_100_100_/_60%)]',
+						'flex gap-10 justify-between pixel-font',
+					)}
+				>
+					<span>{highscore.players.username}</span>
+					<!-- Pad the score with 0's -->
+					<span>{String(highscore.score).padStart(3, '0')}</span>
+				</li>
+			{/each}
+		</ol>
+	</div>
 </div>
