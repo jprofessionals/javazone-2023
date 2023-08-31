aba9fd0fca57b48fef761d835b8e3b3b3a0b8bbe
Marius Kluften <mkluften93@gmail.com>
2023-08-31 16:38:10 +0200

    clean up and simpify header

--- web_app/src/routes/Header.svelte
+++ web_app/src/routes/Header.svelte
@@ -1,30 +1,27 @@
 <script lang="ts">
-	import { currentPlayer } from '$lib/stores'
+	import cn from '$utils/cn'
 	import { AppBar } from '@skeletonlabs/skeleton'
-	import { fade, fly } from 'svelte/transition'
-
-	let currentPlayerName: string | undefined
-	currentPlayer.subscribe((player) => {
-		currentPlayerName = player.name
-	})
+	import { page } from '$app/stores'
 </script>
 
-<AppBar background="bg-black starry">
+<AppBar background="bg-transparent">
 	<svelte:fragment slot="lead">
 		<span class="py-4 text-2xl text-white pixel-font">
 			<span class="text-orange-500">j</span>Pro
 		</span>
 	</svelte:fragment>
-	<div class="flex items-center">
-		{#if currentPlayerName}
-			<div in:fly={{ x: 200, duration: 1000 }} out:fade>
-				<span class="text-center w-full text-slate-800 mx-20 p-2 parch rounded-lg">
-					Player: {currentPlayerName}
+	<svelte:fragment slot="trail">
+		<nav class="text-white color-white pixel-font text-xs">
+			<a href="/">
+				<span class={cn($page.url.pathname === '/' && 'underline underline-offset-4')}>
+					High score
 				</span>
-
-				<button class="btn parch" on:click={() => currentPlayer.set({})}> Exit </button>
-			</div>
-		{/if}
-	</div>
-	<svelte:fragment slot="trail" />
+			</a>
+			<a href="/rules">
+				<span class={cn($page.url.pathname === '/rules' && 'underline underline-offset-4')}>
+					Rules
+				</span>
+			</a>
+		</nav>
+	</svelte:fragment>
 </AppBar>
