f79b0c6ee51e166c1712b937cda3b43cfdb79dd5
Marius Kluften <mkluften93@gmail.com>
2023-08-31 10:00:28 +0200

    improve graphical elements

--- web_app/src/routes/+layout.svelte
+++ web_app/src/routes/+layout.svelte
@@ -1,20 +1,16 @@
 <script lang="ts">
-	// Your selected Skeleton theme:
-	import '@skeletonlabs/skeleton/themes/theme-skeleton.css'
-
-	// This contains the bulk of Skeletons required styles:
-	import '@skeletonlabs/skeleton/styles/skeleton.css'
-
-	// Finally, your application's global stylesheet (sometimes labeled 'app.css')
 	import '../app.css'
 	import Img from '$lib/images/neko-idle.gif'
 
 	import { AppShell } from '@skeletonlabs/skeleton'
 	import Header from './Header.svelte'
 	import Sidebar from '$lib/Sidebar.svelte'
+
+	import { initializeStores } from '@skeletonlabs/skeleton'
+	initializeStores()
 </script>
 
-<AppShell>
+<AppShell slotSidebarLeft="hidden">
 	<svelte:fragment slot="header">
 		<Header />
 	</svelte:fragment>
@@ -26,7 +22,14 @@
 
 	<!-- <svelte:fragment slot="pageFooter">Footer</svelte:fragment> -->
 
-	<div class="absolute -right-4 -bottom-4">
+	<div class="absolute -left-4 -bottom-4" style="transform: rotateY(180deg)">
 		<img src={Img} alt="dog" width="200" />
+	</div>
+	<div class="absolute right-0 bottom-0" style="transform: rotateY(180deg)">
+		<img
+			src="https://www.icegif.com/wp-content/uploads/2023/02/icegif-519.gif"
+			class="h-32"
+			alt="Dog"
+		/>
 	</div>
 </AppShell>
