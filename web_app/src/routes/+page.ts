98a353ba1279c3a440eced549456fc237f66efab
Marius Kluften <mkluften93@gmail.com>
2023-09-04 10:35:12 +0200

    fix prerender bug

--- web_app/src/routes/+page.ts
+++ web_app/src/routes/+page.ts
@@ -1,12 +1,12 @@
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
-export const prerender = true
+export const prerender = false
 
 import type { Score } from '$lib/types'
 import type { PageLoad } from './$types'
 
 export const load: PageLoad = async ({ url }) => {
-	const response = await fetch(`${url.href}api/score?top=10`)
+	const response = await fetch(`${url.href}api/v2/player_score?top=10`)
 	const result = (await response.json()) as { data: Score[]; error: unknown }
 
 	if (result.error) {
